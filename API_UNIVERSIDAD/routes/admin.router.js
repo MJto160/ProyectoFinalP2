3c6d73bcc8204261376583123fbb5910286f4cf0
julioram151005 <jramirezp41@miumg.edu.gt>
2025-10-05 20:58:50 -0600

    Arreglado XD

--- API_UNIVERSIDAD/routes/admin.router.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-    const adminController = require("../controllers/admin.controller.js");
-    const router = require("express").Router();
-    router.post("/create", adminController.create);
-    router.get("/", adminController.findAll);
-    router.put("/update/:id", adminController.update);
-    app.use("/api/admins", router);
-}; 
